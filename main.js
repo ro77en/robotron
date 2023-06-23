2305621535044325791624d47bebc0136ed53fd4
Lucas Maia <lucas.mdantas@icloud.com>
2023-06-23 01:25:47 -0300

    Configurando o click para todas as peças

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const braco = document.querySelector('#braco');
-
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent);
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
     })
 })
 
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('.controle-contador');
+
     if (operacao === '+') {
-        parseInt(braco.value ++);
+        parseInt (peca.value ++);
     } else {
-        parseInt(braco.value --);
+        parseInt (peca.value --);
 
     }
 }
